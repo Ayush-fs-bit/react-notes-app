ba61130797457ac149eb3b6fb497a0b717a779fc
Ayush Deshmukh <deshmukhayush8693@gmail.com>
2026-05-08 21:18:02 +0530

    style: design preview panel and apply consistent color palette across the app

--- src/Preview.js
+++ src/Preview.js
@@ -1,33 +1,51 @@
-const Preview = ({noteSelected,onDelete,onEdit,onArchive,onTagClick}) => {
-  if(!noteSelected){
+const Preview = ({ noteSelected, onDelete, onEdit, onArchive, onTagClick }) => {
+  if (!noteSelected) {
     return <div className="preview">Select A Note</div>
   }
-  function handleTagClick(selectedTag){
+  function handleTagClick(selectedTag) {
     onTagClick(selectedTag)
   }
 
-  function renderTags(tags){
-    if(!tags||tags.length===0)return;
-    return tags.map((t)=>(<div key={t} onClick={(e)=>{e.stopPropagation();
-      handleTagClick(t)}}>#{t}</div>))
+  function renderTags(tags) {
+    if (!tags || tags.length === 0) return;
+    return tags.map((t) => (<div className="preview-tags" key={t} onClick={(e) => {
+      e.stopPropagation();
+      handleTagClick(t)
+    }}>{t}</div>))
   }
-  
-  return ( 
+
+  return (
     <div className="preview">
-      <button onClick={onEdit}>edit</button>
-      <button onClick={onArchive}>
-        {noteSelected.isArchived===true?"Unarchive":"Archive"}
-      </button>
-      <button onClick={onDelete}>Delete</button>
-      <h2>{noteSelected.title}</h2>
-      <p>{noteSelected.content}</p>
-      <div><p>Tags</p>
-        <div className="preview-tags">
-          {renderTags(noteSelected.tags)}
+      <div className="preview-header">
+        <h1>{noteSelected.title}</h1>
+        <div className="preview-btns">
+          <button onClick={onEdit}>edit</button>
+          <button onClick={onArchive}>
+            {noteSelected.isArchived === true ? "Unarchive" : "Archive"}
+          </button>
+          <button onClick={onDelete}>Delete</button></div>
+      </div>
+
+      <div className="note-info">
+        <div className="left">
+          <p>Category</p>
+          <p>Last Modiefied</p>
+          <p>Tags</p>
+        </div>
+        <div className="right">
+          <p>{noteSelected.category}</p>
+          <p>Date</p>
+          <div className="preview-tags-container">
+                {renderTags(noteSelected.tags)}
+          </div>
         </div>
       </div>
+
+
+      <p className="preview-content">{noteSelected.content}</p>
+
     </div>
   );
 }
- 
+
 export default Preview;
